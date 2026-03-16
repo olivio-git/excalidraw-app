230b6cd41c541156df3819af34e847f6bb8f288f
olivio-git <subelzaolivitocabezas@gmail.com>
2026-03-16 13:55:43 -0400

    feat(tauri): configure fs, dialog and window-state plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -55,8 +55,10 @@ async fn get_external_plugins(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_log::Builder::new().level(log::LevelFilter::Warn).build())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![get_external_plugins])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");

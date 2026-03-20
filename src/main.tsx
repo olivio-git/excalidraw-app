3df485c3ab905def9c7c167403472baa68b9373a
olivio-git <subelzaolivitocabezas@gmail.com>
2026-03-20 11:51:08 -0400

    chore(dev): remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "@excalidraw/excalidraw/index.css";
@@ -19,8 +18,4 @@ window.open = (url?: string | URL, target?: string, features?: string) => {
   return _nativeOpen(url, target, features);
 };
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
